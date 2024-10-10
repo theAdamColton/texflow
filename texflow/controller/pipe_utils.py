74e88aa0bb4bc43291854d7f66371f08a9db942f
Adam <atcolton@tutanota.com>
2024-10-10 15:25:41 -0600

    added run pipe function

--- texflow/controller/pipe_utils.py
+++ texflow/controller/pipe_utils.py
@@ -51,7 +51,7 @@ def load_pipe(
 
     if controlnet_models_or_paths is not None:
         controlnets = [
-            ControlNetModel(path, torch_dtype=dtype)
+            ControlNetModel.from_pretrained(path, torch_dtype=dtype)
             for path in controlnet_models_or_paths
         ]
         kwargs["controlnet"] = controlnets
@@ -73,3 +73,67 @@ def set_pipe_type(pipe, type="text2image"):
         return AutoPipelineForInpainting.from_pipe(pipe)
     pipe = _post_pipe_init(pipe)
     return pipe
+
+
+def run_pipe(
+    pipe,
+    prompt=None,
+    negative_prompt=None,
+    image2image_image=None,
+    image2image_strength: float = 0.8,
+    inpainting_image=None,
+    inpainting_mask_image=None,
+    inpainting_strength=None,
+    control_image: list | None = None,
+    height: int = 512,
+    width: int = 512,
+    num_inference_steps: int = 20,
+    guidance_scale: float | None = 7.5,
+    controlnet_conditioning_scale: None | list[float] = None,
+    control_guidance_start: float = 0.0,
+    control_guidance_end: float = 0.0,
+    callback_on_step_end=None,
+    seed: int | None = None,
+):
+    kwargs = dict(
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        height=height,
+        width=width,
+        num_inference_steps=num_inference_steps,
+        guidance_scale=guidance_scale,
+        callback_on_step_end=callback_on_step_end,
+    )
+
+    if hasattr(pipe, "controlnet") and pipe.controlnet is not None:
+        controlnet_kwargs = dict(
+            controlnet_conditioning_scale=controlnet_conditioning_scale,
+            control_guidance_start=control_guidance_start,
+            control_guidance_end=control_guidance_end,
+            control_image=control_image,
+        )
+        kwargs.update(controlnet_kwargs)
+
+    class_name = type(pipe).__name__
+
+    is_img2img = "Img2Img" in class_name
+    if is_img2img:
+        img2img_kwargs = dict(
+            image=image2image_image,
+            strength=image2image_strength,
+        )
+        kwargs.update(img2img_kwargs)
+
+    is_inpainting = "Inpaint" in class_name
+    if is_inpainting:
+        inpainting_kwargs = dict(
+            image=inpainting_image,
+            strength=inpainting_strength,
+            mask_image=inpainting_mask_image,
+        )
+        kwargs.update(inpainting_kwargs)
+
+    if seed is not None:
+        kwargs["generator"] = torch.Generator(pipe.device).manual_seed(seed)
+
+    return pipe(**kwargs)
--- texflow/tests/test_controller.py
+++ texflow/tests/test_controller.py
@@ -1,8 +1,9 @@
+import inspect
 from PIL import Image
 import torch
 import unittest
 
-from ..controller.pipe_utils import load_pipe, set_pipe_type
+from ..controller.pipe_utils import load_pipe, run_pipe, set_pipe_type
 
 
 class TestController(unittest.TestCase):
@@ -12,5 +13,61 @@ class TestController(unittest.TestCase):
             dtype_override=torch.float32,
         )
         output = pipe(prompt="a prompt about dogs", num_inference_steps=2)
+        image = output["images"][0]
+        self.assertIsInstance(image, Image.Image)
+
+    def assertInSignature(self, key, function):
+        self.assertIn(key, inspect.signature(function).parameters.keys())
+
+    def assertNotInSignature(self, key, function):
+        self.assertNotIn(key, inspect.signature(function).parameters.keys())
+
+    def test_set_pipe_type(self):
+        pipe = load_pipe(
+            "hf-internal-testing/tiny-stable-diffusion-pipe",
+        )
+        pipe = set_pipe_type(pipe, type="text2image")
+        self.assertNotInSignature("image", pipe)
+        pipe = set_pipe_type(pipe, type="image2image")
+        self.assertInSignature("image", pipe)
+        pipe = set_pipe_type(pipe, type="inpainting")
+        self.assertInSignature("image", pipe)
+
+    def test_load_controlnet(self):
+        pipe = load_pipe(
+            "hf-internal-testing/tiny-stable-diffusion-pipe",
+            controlnet_models_or_paths=["hf-internal-testing/tiny-controlnet"],
+        )
+        self.assertInSignature("image", pipe)
+        pipe = set_pipe_type(pipe, "image2image")
+        self.assertInSignature("image", pipe)
+        self.assertInSignature("control_image", pipe)
+
+    def test_run_pipe_stable_diffusion_text2image(self):
+        pipe = load_pipe(
+            "hf-internal-testing/tiny-stable-diffusion-pipe",
+            dtype_override=torch.float32,
+        )
+        output = run_pipe(
+            pipe, prompt="rabbit", num_inference_steps=2, height=64, width=64
+        )
+        image = output["images"][0]
+        self.assertIsInstance(image, Image.Image)
+
+    def test_run_pipe_stable_diffusion_image2image(self):
+        pipe = load_pipe(
+            "hf-internal-testing/tiny-stable-diffusion-pipe",
+            dtype_override=torch.float32,
+        )
+        pipe = set_pipe_type(pipe, "image2image")
+        image = (torch.rand(3, 64, 64) * 255).to(torch.uint8)
+        output = run_pipe(
+            pipe,
+            prompt="rabbit",
+            image2image_image=image,
+            num_inference_steps=2,
+            height=64,
+            width=64,
+        )
         image = output["images"][0]
         self.assertIsInstance(image, Image.Image)
